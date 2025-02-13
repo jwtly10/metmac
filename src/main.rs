53df16d4664eb8b99b0854d597bf7497459cd6d8
Josh Watley <watleyjosh@gmail.com>
2025-02-13 19:36:23 +0000

    basic key even listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use env_logger::init;
+use rdev::{listen, Event, EventType, ListenError};
+
+use log::{debug, info};
+
+fn main() -> Result<(), ListenError> {
+    init();
+
+    info!("Starting MetMac...");
+
+    listen(callback)?;
+    Ok(())
+}
+
+fn callback(event: Event) {
+    match event.event_type {
+        EventType::KeyPress(key) => {
+            info!("Key press event: {:?}", key);
+            debug!("Full event defaults: {:?}", event);
+        }
+        _ => {}
+    }
 }
